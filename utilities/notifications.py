ad6340605a2900d0562ba9f8d8238ed16591c2cf
drnoid <beratkir61@gmail.com>
2022-03-03 22:49:20 +0100

    updates

--- utilities/notifications.py
+++ utilities/notifications.py
@@ -1,21 +1,23 @@
 
 from os import environ
 
+import pyperclip
 from models.scrapedproduct import ScrapedProduct
 from persistence.database import session
 from pywhatkit.whats import sendwhatmsg_to_group_instantly
-
-from utilities import args_parser
 
 
 def send_notif(product: ScrapedProduct, group_id: str) -> None:
     p = product
     msg = f"{p.url}\nPrice: {p.item_price}"
+    pyperclip.copy(msg)
     sendwhatmsg_to_group_instantly(
-        group_id=group_id, message=msg, wait_time=7, tab_close=True, close_time=0)
+        group_id=group_id, message="\v", wait_time=7, tab_close=True, close_time=1)
 
 
 def send_notifications() -> None:
+    if environ.get("notifs") != "True":
+        return
     whatsapp_group_id = environ.get("WHATSAPP_GROUP_ID")
 
     if not isinstance(whatsapp_group_id, str):
@@ -24,7 +26,5 @@ def send_notifications() -> None:
         return
 
     products: list[ScrapedProduct] = session.query(ScrapedProduct).all()
-    print(len(products))
     for p in products:
-        print(p)
         send_notif(p, whatsapp_group_id)
