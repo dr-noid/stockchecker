e0ea472d511f07bf5c9c181145e7f4e96afa5f80
drnoid <beratkir61@gmail.com>
2022-03-08 15:28:48 +0100

    added notification support

--- utilities/settings.py
+++ utilities/settings.py
@@ -1,10 +1,15 @@
+from utilities.json_parser import parse_json
+
+
 class _Settings:
     db_reset: bool
     notifications: bool
+    whatsapp_group_id: str
 
     def __init__(self, db_reset=True, notifications=True):
         self.db_reset = db_reset
         self.notifications = notifications
+        self.whatsapp_group_id = str(parse_json("config/env.json"))
 
 
 program_settings = _Settings()
